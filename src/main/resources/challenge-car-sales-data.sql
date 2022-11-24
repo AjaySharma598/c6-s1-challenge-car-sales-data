9325b9d523d04e49f67c1df3e49ee63978105569
@ajaydixit598 <ajaydixit598@gmail.com>
2022-11-24 16:54:49 +0530

    inserted values in the order table

--- src/main/resources/challenge-car-sales-data.sql
+++ src/main/resources/challenge-car-sales-data.sql
@@ -23,3 +23,13 @@ CREATE TABLE `Order`(
 `Order_Date` VARCHAR(10),
 `Customer_Id` int
 );
+
+--insert the value in order table
+INSERT INTO `carsales`.`order` VALUES (3001,123,'01-02-2020',2001);
+INSERT INTO `carsales`.`order` VALUES (3003,187,'02-10-2020',2007);
+INSERT INTO `carsales`.`order` VALUES (3002,100,'30-07-2000',2003);
+INSERT INTO `carsales`.`order` VALUES (3005,201,'9-10-2011',2007);
+INSERT INTO `carsales`.`order` VALUES (3009,145,'10-10-2012',2002);
+INSERT INTO `carsales`.`order` VALUES (3007,167,'02-04-2021',2002);
+INSERT INTO `carsales`.`order` VALUES (3008,189,'02-03-1999',2006);
+INSERT INTO `carsales`.`order` VALUES (3010,100,'18-09-2000',2004);
